b255b0e3419901b9c1bb0b1fd79f5e17cf858867
Pieter Martens <pieter@cg.nl>
2016-09-27 07:45:49 +0200

    add statsd connection

---
+++ config/initializers/statsd.rb
@@ -0,0 +1,6 @@
+# basic configuration per environment for the statsD gem
+StatsD.backend = StatsD::Instrument::Backends::UDPBackend.new("10.0.0.250:8125", :statsd)
+
+# Sample 100% of events. By default all events are reported, which may overload your network or server.
+# You can, and should vary this on a per metric basis, depending on frequency and accuracy requirements
+StatsD.default_sample_rate = (ENV['STATSD_SAMPLE_RATE'] || 1.0 ).to_f
