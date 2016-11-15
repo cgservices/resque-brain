01ee3db4652f56f73cab39cd867fce13debdc1af
Pieter Martens <pieter@cg.nl>
2016-11-15 14:02:54 +0100

    improvements for worker checks

--- lib/monitoring/workers_check.rb
+++ lib/monitoring/workers_check.rb
@@ -2,11 +2,9 @@ module Monitoring
   class WorkersCheck < Monitoring::Checker
     def check!
       @resques.all.map { |resque_instance|
-        resque_instance.workers_running.map { |worker|
-          CheckResult.new(resque_name: resque_instance.name,
-                          check_name: "resque.workers.#{worker[0, worker.index(':')]}",
-                          check_count: 1)
-        }
+        CheckResult.new(resque_name: resque_instance.name,
+                        check_name: 'resque.workers',
+                        check_count: resque_instance.workers_running.count)
       }
     end
   end
