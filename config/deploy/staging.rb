3d0239fe88967bdd4118fe9b0b60091276d8fdef
Pieter Martens <pieter@cg.nl>
2017-07-28 23:10:06 +0200

    maked work on new staging environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,11 +2,13 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 #server 'webruby01.cg.lan', user: 'deployer', runner: 'deployer', group: 'deployer', roles: %w{app web worker}
-server 'webruby02.cg.lan', user: 'deployer', runner: 'deployer', group: 'deployer', roles: %w{app web db}
+#server 'webruby02.cg.lan', user: 'deployer', runner: 'deployer', group: 'deployer', roles: %w{app web db}
+
+server 'accruby02.cg.local', user: 'deploy', runner: 'deploy', group: 'deploy', roles: %w{app web db}
 
 # Asset Server
 # A server with the [worker] role copies the precompiled data from the precompile server
-set :precompile_server, 'webruby02.cg.lan'
+set :precompile_server, 'accruby02.cg.local'
 
 # OTAP environment
 set :rails_env, 'staging'
@@ -15,13 +17,13 @@ set :rails_env, 'staging'
 set :rvm_ruby_string, "ruby-2.2.2#{fetch(:application)}"
 
 # Passenger environment settings
-set :passenger_rvm_ruby_version, 'ruby-2.2.2@global'
+set :passenger_rvm_ruby_version, 'ruby-2.4.0@global'
 
 # Passenger restart command for Passenger version 5
-set :passenger_restart_command, 'passenger-config restart-app'
+#set :passenger_restart_command, 'passenger-config restart-app'
 
 # Passenger restart bug (temporary fix)
-after :'rvm:hook', :'passenger:rvm:after_rvm_path_is_set'
+#after :'rvm:hook', :'passenger:rvm:after_rvm_path_is_set'
 
 # Wich branch will be used for deploy
 set :branch, 'master'
